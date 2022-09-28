bd59bd190f5c478edeb10cf8e0f5f4ced3288d7c
osmanoxil <mr.oussama-yousfi@outlook.fr>
2022-09-28 02:19:25 +0100

    Save

---
+++ Source/BlasterBpCplus/NewRocketMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RocketMovementComponent.h"
+#include "NewRocketMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTERBPCPLUS_API UNewRocketMovementComponent : public URocketMovementComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BlasterBpCplus/RocketMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "RocketMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTERBPCPLUS_API URocketMovementComponent : public UProjectileMovementComponent
+{
+	GENERATED_BODY()
+	
+};
